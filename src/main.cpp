25abfdf985f4502134d78c7b9bc9ff996bd0f2c9
dolev-goaz <www.dolev@gmail.com>
2023-09-21 14:59:53 +0300

    cleanup compile into seperate function

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,27 @@
-#include "../header/file_util.hpp"
-#include <iostream>
+#include <string.h>
 
-int main() {
-    std::string file_contents =  read_file("./code_test.dlv");
+#include "../header/file_util.hpp"
+
+void handle_compile(char* path);
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        std::cerr << "Too few Arguments!" << std::endl
+                  << "Usage: compiler <command> [...args]" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    char* command = argv[1];
+
+    if (strcmp(command, "compile") == 0) {
+        char* path = argv[2];
+        handle_compile(path);
+        exit(EXIT_SUCCESS);
+    }
+    return EXIT_FAILURE;
+}
+
+void handle_compile(char* path) {
+    std::string file_contents = read_file(path);
     std::cout << file_contents << std::endl;
-    return 0;
 }
