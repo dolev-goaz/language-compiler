5191bbcbae253758257658626f6b9aaff2c829b4
dolev-goaz <www.dolev@gmail.com>
2023-09-25 18:30:30 +0300

    parsing and generating code for variable declaration

--- header/generator.hpp
+++ header/generator.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <string>
 
@@ -11,6 +12,8 @@ class Generator {
     std::string generate_program();
 
    private:
+    struct Variable;
+
     void generate_statement(const ASTStatement& statement);
     void generate_exit(const ASTStatementExit& exit_statement);
 
@@ -20,6 +23,18 @@ class Generator {
     std::stringstream m_generated;
     const ASTProgram m_prog;
 
+    // map from variable name to variable details
+    std::map<std::string, Generator::Variable> m_variables;
+    size_t m_stack_size;
+
     struct StatementVisitor;
     struct ExpressionVisitor;
+
+    struct Variable {
+        // stack location in bytes
+        size_t stack_location_bytes;
+
+        // variable size in bytes
+        size_t size_bytes;
+    };
 };
--- header/generator_visitor.hpp
+++ header/generator_visitor.hpp
@@ -6,8 +6,25 @@ struct Generator::StatementVisitor {
 
     void operator()(const ASTStatementExit& exit) const { generator.generate_exit(exit); }
     void operator()(const ASTStatementVar& var_declare) const {
-        std::cerr << "Not Implemented Yet!" << std::endl;
-        exit(EXIT_FAILURE);
+        // check variable name already exists
+        if (generator.m_variables.count(var_declare.name) > 0) {
+            std::cerr << "Identifier " << var_declare.name << "already exists!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        Generator::Variable var = {
+            .stack_location_bytes = generator.m_stack_size,
+            .size_bytes = 8,  // hard coded for now
+        };
+        generator.m_variables.insert({var_declare.name, var});
+        // TODO: need to refer to size_bytes when initializing(push only the required size)
+        if (var_declare.value.has_value()) {
+            generator.generate_expression(var_declare.value.value());
+        } else {
+            // TODO: initialize with 0
+        }
+
+        generator.m_stack_size += var.size_bytes;
     }
 };
 
