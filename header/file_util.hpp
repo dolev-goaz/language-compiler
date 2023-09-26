e3cef65c6d16040e336b65eb91e92c54ac2b592b
dolevgo <www.dolev@gmail.com>
2023-09-26 14:19:10 +0300

    split file_util to header and impl files

--- header/file_util.hpp
+++ header/file_util.hpp
@@ -1,31 +1,9 @@
+#pragma once
+
 #include <fstream>
 #include <iostream>
 #include <vector>
 
-std::string read_file(const std::string& path) {
-    std::ifstream file = std::ifstream(path.c_str());
+std::string read_file(const std::string& path);
 
-    if (!file.is_open()) {
-        std::cerr << "Error opening file "
-                  << "'" << path << "'" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-
-    std::vector<char> buffer;
-    char ch;
-    while (file.get(ch)) {
-        buffer.push_back(ch);
-    }
-
-    file.close();
-
-    std::string res = buffer.data();
-
-    return res;
-}
-
-void write_file(std::string filename, std::string contents) {
-    std::ofstream file = std::ofstream(filename);
-    file << contents;
-    file.close();
-}
+void write_file(std::string filename, std::string contents);
---
+++ src/file_util.cpp
@@ -0,0 +1,29 @@
+#include "../header/file_util.hpp"
+
+std::string read_file(const std::string& path) {
+    std::ifstream file = std::ifstream(path.c_str());
+
+    if (!file.is_open()) {
+        std::cerr << "Error opening file "
+                  << "'" << path << "'" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::vector<char> buffer;
+    char ch;
+    while (file.get(ch)) {
+        buffer.push_back(ch);
+    }
+
+    file.close();
+
+    std::string res = buffer.data();
+
+    return res;
+}
+
+void write_file(std::string filename, std::string contents) {
+    std::ofstream file = std::ofstream(filename);
+    file << contents;
+    file.close();
+}
