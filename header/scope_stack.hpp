f02294a490375ee90f19f8bc62f51ab411c2f533
dolev-goaz <www.dolev@gmail.com>
2024-05-16 22:51:57 +0300

    scope_stack now uses a vector behind-the-scenes to allow better iteration

--- header/scope_stack.hpp
+++ header/scope_stack.hpp
@@ -1,18 +1,18 @@
 #pragma once
 #include <map>
 #include <sstream>
-#include <stack>
 #include <string>
+#include <vector>
 
 template <typename T>
 class ScopeStack {
    public:
     typedef std::map<std::string, T> scope;
-    void enterScope() { scope_stack.push(std::map<std::string, T>()); }
+    void enterScope() { scope_stack.push_back(std::map<std::string, T>()); }
     std::optional<scope> exitScope() {
         if (!scope_stack.empty()) {
-            auto topScope = std::move(scope_stack.top());
-            scope_stack.pop();
+            auto topScope = std::move(scope_stack.back());
+            scope_stack.pop_back();
             return topScope;
         }
         return std::nullopt;
@@ -27,7 +27,7 @@ class ScopeStack {
             exit(EXIT_FAILURE);
         }
 
-        scope& current_scope = scope_stack.top();
+        scope& current_scope = scope_stack.back();
         if (current_scope.count(identifier) > 0) {
             // std::stringstream err_stream;
             // err_stream << "Variable '" << identifier << "' already exists in the current scope.";
@@ -35,19 +35,22 @@ class ScopeStack {
             std::cerr << "Variable '" << identifier << "' already exists in the current scope." << std::endl;
             exit(EXIT_FAILURE);
         }
-        scope_stack.top()[identifier] = variable_data;
+        current_scope[identifier] = variable_data;
     }
     bool lookup(const std::string& identifier, T& variable_data) const {
         // throw error if no existing scope
-        for (auto it = scope_stack.top().rbegin(); it != scope_stack.top().rend(); ++it) {
-            if (it->first == identifier) {
-                variable_data = it->second;
-                return true;
+        for (auto scope_it = scope_stack.rbegin(); scope_it != scope_stack.rend(); ++scope_it) {
+            scope currentScope = *scope_it;
+            for (auto variable_it = currentScope.begin(); variable_it != currentScope.end(); ++variable_it) {
+                if (variable_it->first == identifier) {
+                    variable_data = variable_it->second;
+                    return true;
+                }
             }
         }
         return false;
     }
 
    private:
-    std::stack<scope> scope_stack;
+    std::vector<scope> scope_stack;
 };
