b688720203c1029cde2eba5088dc3f54891ab1b5
dolevgo <www.dolev@gmail.com>
2024-05-26 17:30:09 +0300

    cleanup semantic visitor

--- header/semantic_visitor.hpp
+++ header/semantic_visitor.hpp
@@ -2,81 +2,26 @@
 #include "semantic_analyzer.hpp"
 struct SemanticAnalyzer::ExpressionVisitor {
     SemanticAnalyzer* analyzer;
-    DataType operator()(ASTIdentifier& identifier) const {
-        SymbolTable::Variable literal_data;
-        if (!analyzer->m_symbol_table.lookup(identifier.value, literal_data)) {
-            std::stringstream errorMessage;
-            errorMessage << "Unknown Identifier '" << identifier.value << "'";
-            throw SemanticAnalyzerException(errorMessage.str(), identifier.start_token_meta);
-        }
-        return literal_data.data_type;
-    }
+    DataType operator()(ASTIdentifier& identifier) const { return analyzer->analyze_expression_identifier(identifier); }
 
-    DataType operator()(ASTIntLiteral& ignored) const {
-        (void)ignored;  // suppress unused
-        return DataType::int_64;
+    DataType operator()(ASTIntLiteral& int_literal) const {
+        return analyzer->analyze_expression_int_literal(int_literal);
     }
 
     DataType operator()(const std::shared_ptr<ASTAtomicExpression>& atomic) const {
-        return std::visit(SemanticAnalyzer::ExpressionVisitor{analyzer}, atomic.get()->value);
+        return analyzer->analyze_expression_atomic(atomic);
     }
 
     DataType operator()(const std::shared_ptr<ASTBinExpression>& binExpr) const {
-        // IN THE FUTURE: when there are more types, check for type compatibility
-        auto& lhs = *binExpr.get()->lhs.get();
-        auto& rhs = *binExpr.get()->rhs.get();
-        DataType lhs_data_type = std::visit(SemanticAnalyzer::ExpressionVisitor{analyzer}, lhs.expression);
-        DataType rhs_data_type = std::visit(SemanticAnalyzer::ExpressionVisitor{analyzer}, rhs.expression);
-        // if rhs or lhs are int literals, no need for data narrowing warnings
-        if ((lhs_data_type != rhs_data_type) && (!analyzer->is_int_literal(lhs) && !analyzer->is_int_literal(rhs))) {
-            auto& meta = binExpr.get()->start_token_meta;
-            semantic_warning("Binary operation of different data types. Data will be narrowed.", meta);
-        }
-
-        // type narrowing
-        rhs.data_type = lhs_data_type;
-        lhs.data_type = lhs_data_type;
-        return lhs_data_type;
+        return analyzer->analyze_expression_binary(binExpr);
     }
 
     DataType operator()(const ASTParenthesisExpression& paren_expr) const {
-        auto& inner_expression = *paren_expr.expression.get();
-        return std::visit(SemanticAnalyzer::ExpressionVisitor{analyzer}, inner_expression.expression);
+        return analyzer->analyze_expression_parenthesis(paren_expr);
     }
 
     DataType operator()(ASTFunctionCallExpression& function_call_expr) const {
-        auto& func_name = function_call_expr.function_name;
-        auto& start_token_meta = function_call_expr.start_token_meta;
-        if (analyzer->m_function_table.count(func_name) == 0) {
-            std::stringstream error;
-            error << "Unknown function " << func_name << ".";
-            throw SemanticAnalyzerException(error.str(), start_token_meta);
-        }
-        auto& function_header_data = analyzer->m_function_table.at(function_call_expr.function_name);
-        auto& function_expected_params = function_header_data.parameters;
-        std::vector<ASTExpression>& provided_params = function_call_expr.parameters;
-        if (provided_params.size() != function_expected_params.size()) {
-            std::stringstream error;
-            error << "Function " << func_name << " expected " << function_expected_params.size()
-                  << " parameters, instead got " << provided_params.size() << ".";
-            throw SemanticAnalyzerException(error.str(), start_token_meta);
-        }
-        for (size_t i = 0; i < provided_params.size(); ++i) {
-            provided_params[i].data_type =
-                std::visit(SemanticAnalyzer::ExpressionVisitor{analyzer}, provided_params[i].expression);
-            if (provided_params[i].data_type == function_expected_params[i].data_type) {
-                // no type issues
-                continue;
-            }
-
-            if (!analyzer->is_int_literal(provided_params[i])) {
-                auto& meta = provided_params[i].start_token_meta;
-                semantic_warning("Provided parameter of different data type. Data will be narrowed/widened.", meta);
-            }
-            provided_params[i].data_type = function_expected_params[i].data_type;
-        }
-        function_call_expr.return_data_type = function_header_data.data_type;
-        return function_header_data.data_type;
+        return analyzer->analyze_expression_function_call(function_call_expr);
     }
 };
 
